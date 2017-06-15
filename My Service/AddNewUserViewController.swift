3498775373a9a25d95b58ff36ff842964a9797f4
student04 <smeesathit@gmail.com>
2017-06-15 16:58:49 +0700

    Check empty input complete

--- My Service/AddNewUserViewController.swift
+++ My Service/AddNewUserViewController.swift
@@ -10,6 +10,10 @@ import UIKit
 
 class AddNewUserViewController: UIViewController {
     
+    // Field declaration
+    var nameString: String? = nil
+    var userString: String? = nil
+    var passwordString: String? = nil
     
     @IBOutlet weak var nameTextField: UITextField!
     
@@ -19,7 +23,24 @@ class AddNewUserViewController: UIViewController {
     
     
     @IBAction func saveButton(_ sender: Any) {
-    } 
+        // Get value from TextField
+        nameString = nameTextField.text
+        userString = userTextField.text
+        passwordString = passwordTextField.text
+        
+        // check empty input
+        let intName = nameString?.characters.count
+        let intUser = userString?.characters.count
+        let intPassword = passwordString?.characters.count
+        
+        if (intName == 0) || (intUser == 0) || (intPassword == 0) {
+            // Have empty input
+            print("Have empty input!!!")
+        } else {
+            print("No empty input!!!")
+        } // End if-else
+        
+        } // End saveButton method
     
 
     override func viewDidLoad() {
